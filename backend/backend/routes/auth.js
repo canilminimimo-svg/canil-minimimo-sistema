e2cc3cb12b75943b2e5cc544bd39eabe519b36d8
canilminimimo-svg <canilminimimo@gmail.com>
2026-02-08 21:46:13 -0300

    Update auth.js

--- backend/backend/routes/auth.js
+++ backend/backend/routes/auth.js
@@ -1,25 +1,36 @@
+const supabase = require('../config/supabase');
 const express = require('express');
 const router = express.Router();
 
 
 // LOGIN SIMPLES (BASE)
 router.post('/login', async (req, res) => {
-const { email, senha } = req.body;
+  const { email, senha } = req.body;
 
+  if (!email || !senha) {
+    return res.status(400).json({ erro: 'Email e senha obrigatórios' });
+  }
 
-if (!email || !senha) {
-return res.status(400).json({ erro: 'Email e senha obrigatórios' });
-}
+  try {
 
+    const { data, error } = await supabase
+      .from('usuarios')
+      .select('*')
+      .eq('email', email)
+      .eq('senha', senha)
+      .single();
 
-// Aqui depois vamos conectar com banco Supabase
+    if (error || !data) {
+      return res.status(401).json({ erro: 'Usuário ou senha inválidos' });
+    }
 
+    return res.json({
+      mensagem: 'Login realizado com sucesso',
+      usuario: data
+    });
 
-return res.json({
-mensagem: 'Login base funcionando',
-usuario: email
+  } catch (err) {
+    return res.status(500).json({ erro: 'Erro no servidor' });
+  }
+
 });
-});
-
-
-module.exports = router;
